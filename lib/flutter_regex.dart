313af13f00fceb3549e4a54ffc808cec254b930e
Shirsh Shukla <shirsh94@gmail.com>
2024-02-22 19:43:30 +0530

    Fix code formatting

--- lib/flutter_regex.dart
+++ lib/flutter_regex.dart
@@ -1,4 +1,5 @@
 library flutter_regex;
+
 export 'package:flutter_regex/src/flutter_regex_pattern.dart';
 export 'package:flutter_regex/src/flutter_regex_validator.dart';
 export 'package:flutter_regex/src/flutter_regex_extension.dart';
